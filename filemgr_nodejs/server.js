a9064abf60ceb89c203c9db08af8e0cea8449a03
Lucas Zunho <lucaszunho17@gmail.com>
2022-08-16 16:46:14 -0300

    Added server upload handler

--- filemgr_nodejs/server.js
+++ filemgr_nodejs/server.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+const multer = require("multer");
+const upload = multer({ dest: "uploaded/" });
 
 const HOST = '127.0.0.1';
 const PORT = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.post("/uploaded", uploadFiles);
+
+app.post("/fileupload", upload.array("files"), uploadFiles);
 
 function uploadFiles(req, res) {
     console.log(req.body);
+    console.log(req.files);
+    res.json({ message: "Upload done!!" });
 }
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(PORT, () => {
