d63ded3b5a26e1d7b12d16d12f9a889234517804
Lucas Zunho <lucaszunho17@gmail.com>
2023-05-06 14:22:13 -0300

    Funcionalidades dos cards & Melhor fluxo de código

--- plain_startpage/js/main.js
+++ plain_startpage/js/main.js
@@ -1,60 +1,48 @@
-function updateItensCollection() {
-    for (var i in localStorage) {
-        if (localStorage.getItem(i)) {
-            let itemData = (localStorage.getItem(i)).split(";");
-            console.log(localStorage.getItem(i));
-
-            // item view card
-            favourites_obj = document.getElementById('favourites-wrapper');
-            var div = document.createElement('div');
-            div.className = 'favourite';
-            div.innerHTML = "<label for=\"" + i + "\">" + itemData[0] + "</label> => <a id=\"" + i + "\" href=\"" + itemData[1] + "\">" + itemData[1] + "</a>";
-            favourites_obj.appendChild(div);
-        }
-    }
-}
-
 window.onload = function () {
-    updateItensCollection();
-
+    let resetBtn = document.getElementById('reset-btn');
     let itemForm = document.getElementById('add-item-form');
     let itemName = document.getElementById('item-name');
     let itemUrl = document.getElementById('item-url');
-
-    itemForm.addEventListener('submit', event => {
-        event.preventDefault();
-
-        let itemNameInput = itemName.value;
-        let itemUrlInput = itemUrl.value;
-
-        // Gerando hash do nome
-        let itemHash = md5(itemNameInput + itemUrlInput);
-
-        // Adicionando valores ao localStorage
-        if (!localStorage.getItem(itemHash)) {
-            console.log("Item => ['" + itemNameInput + "', '" + itemUrlInput + "', '" + itemHash + "']");
-            localStorage.setItem(itemHash, itemNameInput + ";" + itemUrlInput);
+    
+    if (storageVerify()) {
+        resetBtn.addEventListener('click', event => {
+            localStorage.clear();
             updateItensCollection();
-        } else {
-            alert("Item já existe!");
-        }
+        });
 
-        window.location = "#close";
-    });
+        itemForm.addEventListener('submit', event => {
+            event.preventDefault();
 
-    itemName.addEventListener("input", (event) => {
-        if (itemUrl.validity.typeMismatch) {
-            itemUrl.setCustomValidity("Adicione um nome válido!");
-        } else {
-            itemUrl.setCustomValidity("");
-        }
-    });
+            let itemNameInput = itemName.value;
+            let itemUrlInput = itemUrl.value;
 
-    itemUrl.addEventListener("input", (event) => {
-        if (itemUrl.validity.typeMismatch) {
-            itemUrl.setCustomValidity("Uma URL é esperada para esse campo!");
-        } else {
-            itemUrl.setCustomValidity("");
-        }
-    });
+            // Gerando hash do nome
+            let itemHash = md5(itemNameInput + itemUrlInput);
+
+            // Adicionando valores ao localStorage
+            addToDatabase(itemNameInput, itemUrlInput, itemHash);
+            updateItensCollection();
+
+            // Resetando o formulário & fade-out do modalbox
+            itemName.value = '';
+            itemUrl.value = '';
+            window.location = "#close";
+        });
+
+        itemName.addEventListener("input", (event) => {
+            if (itemUrl.validity.typeMismatch) {
+                itemUrl.setCustomValidity("Adicione um nome válido!");
+            } else {
+                itemUrl.setCustomValidity("");
+            }
+        });
+
+        itemUrl.addEventListener("input", (event) => {
+            if (itemUrl.validity.typeMismatch) {
+                itemUrl.setCustomValidity("Uma URL é esperada para esse campo!");
+            } else {
+                itemUrl.setCustomValidity("");
+            }
+        });
+    } 
 }
--- plain_startpage/js/storage.js
+++ plain_startpage/js/storage.js
@@ -1,6 +1,6 @@
 /* window data object size verification */
 function storageAvailable(type) {
-    let storage;
+    var storage;
     try {
         storage = window[type];
         const x = "__storage_test__";
@@ -8,6 +8,7 @@ function storageAvailable(type) {
         storage.removeItem(x);
         return true;
     } catch(e) {
+        console.error(e);
         return (
             e instanceof DOMException &&
             (e.code === 22 || e.code === 1014 ||
@@ -18,22 +19,63 @@ function storageAvailable(type) {
     }
 }
 
-window.onload = function() {
-    // Testing localStorage
-    if (storageAvailable("localStorage")) {
+function emptyMsgHandler() {
+    console.log(localStorage.length);
+    favourites_obj = document.getElementById('favourites-wrapper');
+    favourites_obj.innerHTML = "";
+    if (localStorage.length == 0) {
+        // empty card message
+        var div = document.createElement('div');
+        div.style.color = 'white';
+        div.style.textAlign = 'center';
+        div.style.fontSize = '14px';
+        div.innerHTML = '<p>Para adicionar novos card utilize o botão de favoritos</p>';
+        favourites_obj.appendChild(div);
+    }
+}
 
+function updateItensCollection() {
+    emptyMsgHandler();
+    for (var i = 0, len = localStorage.length; i < len; ++i) {
+        if (localStorage.getItem(localStorage.key(i))) {
+            let itemData = (localStorage.getItem(localStorage.key(i))).split(";");
+
+            // item view card
+            favourites_obj = document.getElementById('favourites-wrapper');
+            let div = document.createElement('div');
+            div.className = 'favourite';
+            div.innerHTML = "<a id=\"" + localStorage.key(i) + "\" href=\"" + itemData[1] + "\" target=\"_blank\">" + itemData[0] + "</a>";
+            favourites_obj.appendChild(div);
+
+            // Update index number
+            window.viewCardCount += 1;
+        }
+    }
+}
+
+function storageVerify() {
+    if (storageAvailable("localStorage")) {
+        updateItensCollection();
+        return true;
     } else {
         // Console error
         console.error("Unable to use localStorage storage type!");
 
         // Site error
         favourites_obj = document.getElementById('favourites-wrapper');
-        favourites_obj.appendChild(document.createElement('div')); // empty div (1st block)
-
         var div = document.createElement('div');
         div.className = 'site-error';
         div.innerHTML = '<h5>Unable to use localStorage! Please, enable it...</h5>';
-        favourites_obj.appendChild(div); // Error msg block
-        throw "";
+        favourites_obj.appendChild(div);
+        return false;
+    }
+}
+
+function addToDatabase(itemNameInput, itemUrlInput, itemHash) {
+    if (!localStorage.getItem(itemHash)) {
+        console.log("Item => ['" + itemNameInput + "', '" + itemUrlInput + "', '" + itemHash + "']");
+        localStorage.setItem(itemHash, itemNameInput + ";" + itemUrlInput);
+    } else {
+        alert("Item já existe!");
     }
 }
